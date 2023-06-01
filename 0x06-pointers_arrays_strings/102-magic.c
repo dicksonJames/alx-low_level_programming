a5bd71d4ef1ef145356774e8d7ae61e95106e8b3
dicksonJames <dicksonjamesabi@gmail.com>
2023-06-01 12:00:24 -0700

    update

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
   * main - Entry point
   * Description: a short description of the main function
@@ -22,7 +21,7 @@ p = &n;
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-*(p + 5) = 98;
+;
   /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
